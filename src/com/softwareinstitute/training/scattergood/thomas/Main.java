316ffda825a4740c561732ecc8c0ee5304dae53f
Thomas Scattergood <tscattergood@softwareinstitute.com>
2021-10-12 16:03:39 +0100

    I added lines of code

--- src/com/softwareinstitute/training/scattergood/thomas/Main.java
+++ src/com/softwareinstitute/training/scattergood/thomas/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-	int choice = 1;
+    int choice = (int) (Math.random()+2);
     String message;
+
+    System.out.println(choice);
     if (choice == 1){
         message = "Happy Birthday";
     }
@@ -15,8 +17,16 @@ public class Main {
     System.out.println(message);
 
     message = (choice == 1) ? "Happy Christmas" : "You have been bad, no presents";
-    String string = new String((choice == 1) ? "Happy Christmas" : "You have been bad, no presents");
+    String string = ((choice == 1) ? "Happy Christmas" : "You have been bad, no presents");
     System.out.println(message);
     System.out.println(string);
+
+    int num = 2;
+        switch (choice) {
+            case 0 -> System.out.println("Case is 0");
+            case 1 -> System.out.println("Case is 1");
+            case 2 -> System.out.println("Case is 2");
+            default -> System.out.println("Case is 3");
+        }
     }
 }
