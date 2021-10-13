cd6a67fd84c50da9d262e2ccd1ab00777648167f
Thomas Scattergood <tscattergood@softwareinstitute.com>
2021-10-13 17:39:11 +0100

    I added sections of code with examples from lecture

--- src/com/softwareinstitute/training/scattergood/thomas/Main.java
+++ src/com/softwareinstitute/training/scattergood/thomas/Main.java
@@ -1,9 +1,11 @@
 package com.softwareinstitute.training.scattergood.thomas;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-
+    //if loops and switch
     int choice = (int) (Math.random()*10);
     String message;
 
@@ -38,15 +40,16 @@ public class Main {
             }
             System.out.println(count++);
         }
+        //do while loops
         count=0;
         do{
             if (count>10){
-                playAgain =true;
+                playAgain = true;
             }
             System.out.println(++count);
         }while(!playAgain);
 
-
+        //for loops and enhanced for loops
         for (int i = 1; i<10; i++) {
             System.out.println("I have " +i+ " friends");
         }
@@ -59,6 +62,75 @@ public class Main {
         for(int k:enhancedFor) {
             System.out.println(k);
         }
+
+        //arrays
+        String[] paints = new String[6];
+        System.out.println(paints.length);
+        paints [4] = "Dulux";
+        paints [1] = "HomeBase";
+        paints[2] = "B&Q";
+        paints[5] = "Paint";
+
+
+        for(String s : paints){
+            System.out.println(s);
+        }
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        //multi dimensional arrays (2d grid example)
+        int count1 = 0;
+        int grid_counter[][] = new int[15][12];
+
+        for(int i = 0; i < grid_counter.length; i++){
+            for(int j =0; j<grid_counter[i].length; j++){
+                grid_counter[i][j] = ++count1;
+                System.out.print(grid_counter[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+
+        ArrayList<String> techClass = new  ArrayList<String>(10);
+        techClass.add("Tom");
+        techClass.add("Will");
+        techClass.add("Gabriel");
+        techClass.add("Louis");
+        techClass.add("Arnold");
+        techClass.add(1, "Ryan");
+
+        System.out.println(techClass.size());
+        System.out.println(techClass.get(3));
+
+        System.out.println(" ");
+
+            for(String name1: techClass){
+                System.out.println(name1);
+
+            }
+
+        System.out.println(tellTheClassAPerson(0));
+
+    }
+
+    public static String tellTheClassAPerson(int whichPerson){
+        String name = "";
+        switch (whichPerson){
+            case 1:
+                name = "Thomas";
+                break;
+            case 2:
+                name = "William";
+                break;
+            default:
+                name = "Mary";
+                break;
+        }
+                return name;
 
     }
 
